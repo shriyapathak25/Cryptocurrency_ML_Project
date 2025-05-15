053fb2a42ec9e9f39443204fbcb2deae57036ef3
Saniya-126 <saniyapacha@gmail.com>
2025-05-15 19:50:32 +0530

    Resolved merge conflicts

--- flask_app/app.py
+++ flask_app/app.py
@@ -42,6 +42,6 @@ def predict():
         return render_template('index.html',
                                prediction_text=f"⚠️ Unexpected error: {str(e)}")
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
